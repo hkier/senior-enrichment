98f60e310b6e766f49b10224dddc83126600258b
Howard Kier <howard@Howards-MacBook-Air.local>
2017-08-30 16:01:21 -0500

    Long Time, no Save

--- server/start.js
+++ server/start.js
@@ -3,8 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {resolve} = require('path')
-
-
+const path = require('path')
 const pkg = require('../package.json')
 
 const app = express()
@@ -25,9 +24,11 @@ module.exports = app
   .use(bodyParser.json())
   .use(express.static(resolve(__dirname, '..', 'public'))) // Serve static files from ../public
   // .use('/api', require('./api')) // Serve our api
+  .use('/bootstrap', express.static('../node_modules/bootstrap/dist'))
+  .use('/jquery', express.static('../node_modules/jquery/dist'))
   .use('/api', require('./app/routes'))
   .use('/', require('./app/routes'))
-  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html'))) // Send index.html for any other requests.
+  // .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html'))) // Send index.html for any other requests.
   // notice the use of `_` as the first parameter above. This 
   // is a pattern for parameters that must exist, but you 
   // don't use or reference (or need) in the function body 
